68c47f70250f6100fd92c00c680e3c902c8a0503
Li Wangsheng <op77@qq.com>
2022-03-18 23:09:08 +0800

    链表加法

--- 2.两数相加/1单数+进位.cpp
+++ 2.两数相加/1单数+进位.cpp
@@ -8,6 +8,12 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//虽然逻辑有点复杂，但是能ac感觉良好
+/*
+思路：对我来说，难点主要在有进位的情况下，只有单个链表有元素和两个链表都没元素
+的时候应该怎么办，于是我又创建了个函数专门处理一个链表和进位的相加。
+if太多，逻辑很混乱
+*/
 class Solution {
 public:
     int flag = 0;
