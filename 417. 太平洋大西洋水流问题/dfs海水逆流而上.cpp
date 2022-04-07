730b72245f0fe4927f4d7fe589c377e75241495c
Fresh-up <op77@qq.com>
2022-04-07 11:50:07 +0800

    加点注释

--- 417. 太平洋大西洋水流问题/dfs海水逆流而上.cpp
+++ 417. 太平洋大西洋水流问题/dfs海水逆流而上.cpp
@@ -16,13 +16,14 @@ public:
         vector<vector<bool>> reacha(m, vector<bool>(n, false));
 
         for (int i = 0; i < m; ++i){
-            dfs(heights, i, 0, reachp);
-            dfs(heights, i, n-1, reacha);
+            dfs(heights, i, 0, reachp);//从矩阵的左边入手
+            dfs(heights, i, n-1, reacha);//从矩阵的右边入手
         }
         for (int j = 0; j < n; ++j){
-            dfs(heights, 0, j, reachp);
-            dfs(heights, m-1, j, reacha);
+            dfs(heights, 0, j, reachp);//从矩阵的上边入手
+            dfs(heights, m-1, j, reacha);//从矩阵的下边入手
         }
+        //对两个可达矩阵取交集，即海水可以倒流到的点
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
                 if (reacha[i][j] && reachp[i][j]){
@@ -35,16 +36,20 @@ public:
         return place;
     }
     void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<bool>>& canreach){
-        if(canreach[i][j]){
+        if(canreach[i][j]){//true false矩阵每个节点隐含着两层意思
+        //一，这个点可以流入一个海洋    二，这个点已经被访问过
             return;
         }
         canreach[i][j] = true;
         int x, y;
+
+        //开始向四周搜索
         for (int k = 0; k < 4; ++k){
             x = i + direction[k], y = j + direction[k+1];
             if (x >= 0 && x < heights.size() && 
                 y >= 0 && y < heights[0].size() && 
                 heights[x][y] >= heights[i][j])//别忘了等号
+                //能进入下一个递归的，就已经说明海水已经可以倒流到即将要递归的这个点的
                 dfs(heights, x, y, canreach);
         }
     }
