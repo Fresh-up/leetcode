5b0269b6bf418a2dbfb2c70c62c2429e7329ee8c
Fresh-up <op77@qq.com>
2022-05-14 15:41:48 +0800

    dfs+字典树

---
+++ 208. 实现 Trie (前缀树)/字典树.cpp
@@ -0,0 +1,43 @@
+class Trie {
+private:
+    vector<Trie*> children;
+    bool isEnd;
+
+    Trie* searchPrefix(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                return nullptr;
+            }
+            node = node->children[ch];
+        }
+        return node;
+    }
+
+public:
+    Trie() : children(26), isEnd(false) {}
+
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                node->children[ch] = new Trie();
+            }
+            node = node->children[ch];
+        }
+        node->isEnd = true;
+    }
+    // 它是每个字符就占用26个位置，一直向下生长。存储apple就要用到5 * 26个位置
+
+    bool search(string word) {
+        Trie* node = this->searchPrefix(word);
+        return node != nullptr && node->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        return this->searchPrefix(prefix) != nullptr;
+    }
+};
+// O(1) O(n*26) n是所有插入字符串的长度之和
