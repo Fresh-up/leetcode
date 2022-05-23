f40f3f20cf8cc6354fe45c42e928816a65823443
Fresh-up <op77@qq.com>
2022-05-23 09:20:59 +0800

    二分

---
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字/二分.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > mid) r = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
+// O(logn) O(1)
