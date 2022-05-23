42a8c2c7603f8f59a748a0344d06baf780bf4800
Fresh-up <op77@qq.com>
2022-05-23 11:19:39 +0800

    双指针

--- 125. 验证回文串/双指针.cpp
+++ 125. 验证回文串/双指针.cpp
@@ -5,7 +5,7 @@ public:
         while (l <= r && !isalnum(s[l])) ++l;
         if (l > r) return true;
         while (l <= r) {
-            while (l < r && !isalnum(s[l])) ++l;
+            while (l < r && !isalnum(s[l])) ++l;// 没用l <= r，防止"a a"的情况
             while (l < r && !isalnum(s[r])) --r;
             if (tolower(s[l++]) != tolower(s[r--])) return false;
         }
